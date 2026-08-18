442552a2d3e91fac21e1415caf22cb1181c5b0b2
IceSolidus <theo.bellomia@gmail.com>
2026-08-18 20:01:18 +0200

    a

---
+++ Test Game Jam/icon/data/Tiled/Asset/tmw_Dungeon_A1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tmw_Dungeon_A1" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="391" columns="23">
+ <image source="../../../img/tilesets/Dungeon_A1.png" width="768" height="576"/>
+ <tile id="0" type="1"/>
+</tileset>
